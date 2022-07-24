085eace7141148116cbfb4098d88ff0cf904c225
mdodell <dodellmitchell@gmail.com>
2022-07-24 13:19:24 -0400

    Showcase ts issue

---
+++ environment.d.ts
@@ -0,0 +1,16 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV:
+        | "development"
+        | "production"
+        | "testing"
+        | "staging"
+        | "preview";
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
